814514ed87cc6735d2e1ea312d281c375371d146
JunKoding <wnsrb0407@daum.net>
2022-04-14 21:16:02 +0900

    2022.04.14 - 블루포인트(수원 공식 팬 스토어), 경기 예매 url 연결

--- SuwonSamsung/ViewController.swift
+++ SuwonSamsung/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         let gameTimeUrl = NSURL(string: "https://m.sports.naver.com/kfootball/schedule/team?category=kleague&date=20220505&teamCode=02")
         let gameTimeSafariView: SFSafariViewController = SFSafariViewController(url: gameTimeUrl! as URL)
         self.present(gameTimeSafariView, animated: true, completion: nil)
-    } // 경기 일정 페이지로 가는 버튼
+    } // 버튼에 경기 일정 url 연결
     
     
     @IBAction func lineUp(_ sender: UIButton) {
@@ -41,12 +41,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func goOfficialWeb(_ sender: UIButton) {
-        // 버튼에 공식 사이트 연결
-        
-        let officialUrl = NSURL(string: "http://www.bluewings.kr")
+        // 버튼에 공식 url 연결
+        let officialUrl = NSURL(string: "http://www.bluewingsshop.com")
         let officialSafariView: SFSafariViewController = SFSafariViewController(url: officialUrl! as URL)
         self.present(officialSafariView, animated: true, completion: nil)
     }
+    
+    @IBAction func ticket(_ sender: UIButton) {
+        // 버튼에 경기 예매 url 연결
+        let ticketUrl = NSURL(string: "http://ticket.interpark.com/m-ticket/Sports/GoodsInfo?SportsCode=07002&TeamCode=PS003")
+        let ticketSafariView: SFSafariViewController = SFSafariViewController(url: ticketUrl! as URL)
+        self.present(ticketSafariView, animated: true, completion: nil)
+    }
+    
     
 }
 
