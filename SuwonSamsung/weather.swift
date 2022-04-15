488c971d3a7459b1c058cd66fe29bbdb4b9da66e
JunKoding <wnsrb0407@daum.net>
2022-04-15 18:37:43 +0900

    2022.04.15 - 경기장 날씨

---
+++ SuwonSamsung/weather.swift
@@ -0,0 +1,10 @@
+//
+//  weather.swift
+//  SuwonSamsung
+//
+//  Created by JunKyu Lee on 2022/04/15.
+//
+
+import Foundation
+
+let appKey = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
