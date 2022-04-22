ccd8b765a7a54924b387fe39040a4c09c7a8335d
JunKoding <wnsrb0407@daum.net>
2022-04-22 21:12:24 +0900

    2022.04.22 - 경기장 날씨 수정

--- SuwonSamsung/page/bigbirdForecastViewController.swift
+++ SuwonSamsung/page/bigbirdForecastViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class bigbirdForecastViewController: UIViewController {
-
     
     @IBAction func dissmissBigbirdForecast(_ sender: UIButton) {
         presentingViewController?.dismiss(animated: true, completion: nil)
@@ -17,18 +16,5 @@ class bigbirdForecastViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
